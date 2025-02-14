c0dda1804417cd6a27c85dfb0c509629717c2e0b
jzzlyghyzzly <108499079+jzzlyghyzzly@users.noreply.github.com>
2025-02-14 17:03:50 +0900

    [Bronze I] Title: 진법 변환 2, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int B = Integer.parseInt(input[1]);
+		System.out.println(Integer.toString(N,B).toUpperCase());
+	}
+
+}
