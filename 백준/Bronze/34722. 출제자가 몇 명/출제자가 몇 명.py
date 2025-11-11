5292399a1669db94290a40518a6e6f4f4353b950
jzzlyghyzzly <108499079+jzzlyghyzzly@users.noreply.github.com>
2025-11-11 18:56:09 +0900

    [Bronze IV] Title: 출제자가 몇 명, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34722. 출제자가 몇 명/출제자가 몇 명.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+N = int(input().strip())
+count = 0
+for _ in range(N):
+    s, c, a, r = map(int, input().split())
+    if s >= 1000 or c >= 1600 or a >= 1500 or (1 <= r <= 30):
+        count += 1
+print(count)
