1bcd459b14fdb0bcc0ff9def629c9de04d034066
jzzlyghyzzly <108499079+jzzlyghyzzly@users.noreply.github.com>
2026-03-22 19:21:06 +0900

    [Silver V] Title: 나이순 정렬, Time: 224 ms, Memory: 46020 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,11 @@
+import sys
+input= sys.stdin.readline
+n = int(input().strip())
+people =[]
+for _ in range(n):
+    age, name= input().split()
+    age = int(age)
+    people.append((age, name))
+people.sort(key = lambda x: x[0])
+for age, name in people:
+    print(age, name)
